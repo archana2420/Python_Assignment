82fe39cf46ef23b74975f701a2a498c0471ec004
Archana S Nair-ENG18CS0044 <65852760+archana2420@users.noreply.github.com>
2022-01-17 16:03:39 +0530

    added comments

--- find_roots.py
+++ find_roots.py
@@ -1,12 +1,12 @@
 import math
 def find_roots(a,b,c):
-    discriminant=math.sqrt((b*b)-(4*a*c))
+    discriminant=math.sqrt((b*b)-(4*a*c)) 
     x1=((-b)+discriminant)/(2*a)
     x2=((-b)-discriminant)/(2*a)
     return x1,x2
 
 
-if __name__=="__main__":
+if __name__=="__main__": #  Quadratic equation is in the form ax2 + bx + c = 0 , we need to get its coefficients i.e. a,b,c
     a=int(input("Enter a's value:"))
     b=int(input("Enter b's value:"))
     c=int(input("Enter c's value:"))
